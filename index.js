956c5bf1f378190d261b32d11e61fba53da4c065
ahmadsm <pioner404>
2020-03-25 11:03:39 +0700

    repair heroku

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ var express = require('express');
 // initialize app
 var app = express();
 
+app.use(express.static(__dirname));
+
 app.get('/', function (request, response) {
-    response.send({message: 'hello express'});
+    // response.send({message: 'hello express'});
+    response.render("index");
 });
 
 app.listen(3000, function () {
